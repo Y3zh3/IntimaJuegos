9bba135f7229343f79c8dc4df679cedc01850506
Melina yessenia Hernández ñahuis <y3zh31409@gmail.com>
2025-08-10 04:07:07 +0000

    no se centra

--- src/app/guia-para-parejas/page.tsx
+++ src/app/guia-para-parejas/page.tsx
@@ -160,9 +160,9 @@ export default function GuiaParaParejas() {
       </header>
       <main className="flex-1">
         <section className="w-full py-12 md:py-16">
-          <div className="container flex flex-col items-center px-4 text-center md:px-6">
+          <div className="container px-4 text-center md:px-6">
             <h1 className="text-4xl font-bold tracking-tighter text-primary sm:text-6xl md:text-5xl">Guía para Parejas</h1>
-            <p className="mx-auto max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             Consejos, ideas y recursos para fortalecer la conexión y la intimidad en tu relación.
             </p>
           </div>
