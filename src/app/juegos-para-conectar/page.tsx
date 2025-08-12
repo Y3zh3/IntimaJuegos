0d4ce1f5b94b60a809ca35049c4c7f9576b4ba80
Melina yessenia Hernández ñahuis <y3zh31409@gmail.com>
2025-08-12 06:13:58 +0000

    https://images.ecestaticos.com/kL4cRkcIccu47bmz2ZU4SgifghA=/0x87:1697x10

--- src/app/juegos-para-conectar/page.tsx
+++ src/app/juegos-para-conectar/page.tsx
@@ -64,7 +64,7 @@ export default function JuegosParaConectar() {
                 <Link href="/juegos-para-conectar">
                   <div className="relative h-[450px] w-full overflow-hidden">
                     <Image
-                      src="https://cdn0.uncomo.com/es/posts/1/1/5/ideas_atrevidas_para_hacer_con_tu_pareja_49511_600.jpg"
+                      src="https://images.ecestaticos.com/kL4cRkcIccu47bmz2ZU4SgifghA=/0x87:1697x1041/1600x900/filters:fill(white):format(jpg):quality(99)/f.elconfidencial.com/original/93c/d92/139/93cd921390f261b41391cdd29ef2e81e.jpg"
                       alt="Banner de juguetes sexuales"
                       layout="fill"
                       objectFit="cover"
